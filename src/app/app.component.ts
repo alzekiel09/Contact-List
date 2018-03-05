2090aabf8c6782f1557515a392515c28d2f7a202
alzekiel09 <aixonairashii@gmail.com>
2018-03-06 00:55:28 +0800

    adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  address: string;
+}
+interface PostId extends Post {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  postsCol: AngularFirestoreCollection<Post>;
+  contact: any;
+
+  firstName: string;
+  lastName: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  address: string;
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+  
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('contact');
+    this.contact = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      })
+  }
+
+  addPost() {
+    this.afs.collection('contact').add({'firstName': this.firstName,'lastName': this.lastName,'phone': this.phone,
+	'mobile': this.mobile, 'email': this.email,'address': this.address
+	});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('contact/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('contact/'+postId).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDbebJpiLCYRFJlIaovhKx1WEFj01Etncw",
+    authDomain: "firestore-f61c2.firebaseapp.com",
+    databaseURL: "https://firestore-f61c2.firebaseio.com",
+    projectId: "firestore-f61c2",
+    storageBucket: "firestore-f61c2.appspot.com",
+    messagingSenderId: "687308772707"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
